fa8fe3d3892dbe9d6acfa737f3262f5f353038b3
Tobias Ask <tobiasask@microsoft.com>
2022-12-22 14:34:41 +0100

    Correct part 1, simpler algorithm

--- dag 15/solution.py
+++ dag 15/solution.py
@@ -1,11 +1,3 @@
-ALL_DIRECTIONS = [
-    (1, 0),
-    (0, 1),
-    (-1, 0),
-    (0, -1)
-]
-
-
 def get_object_position(report_part, offset_in_part):
     x_part, y_part = report_part.split()[offset_in_part:]
     return int(x_part.split('=')[1][:-1]), int(y_part.split('=')[1])
@@ -22,45 +14,35 @@ def get_manhattan_distance(point, other_point):
     return abs(x - other_x) + abs(y - other_y)
 
 
-def get_covered_points(sensor_position, radius, row):
-    points = set()
-    queue = [sensor_position]
-    visited = set()
-    while queue:
-        point = queue.pop()
-        x, y = point
-        visited.add(point)
-        if y == row:
-            points.add(point)
-            
-        directions = ALL_DIRECTIONS if y != row else [(1, 0), (-1, 0)]
-
-        for direction in directions:
-            delta_x, delta_y = direction
-            new_point = x + delta_x, y + delta_y
-            if (get_manhattan_distance(sensor_position, new_point) <= radius and
-                    new_point not in visited):
-                queue.append(new_point)
-
-    return points
-
-
 def part1():
-    coverage = set()
     beacon_positions = set()
     sensor_positions = []
     radii = []
+    row = 2000000
 
     with open('dag 15/input.txt') as sensor_reports:
         for sensor_report in sensor_reports:
             sensor_position, beacon_position = get_positions(sensor_report)
             beacon_positions.add(beacon_position)
             radius = get_manhattan_distance(sensor_position, beacon_position)
-            # covered_points = get_covered_points(sensor_position, radius, 2000000)
-            # coverage.update(covered_points)
+            sensor_positions.append(sensor_position)
+            radii.append(radius)
 
-    # coverage.difference_update(beacon_positions)
-    # print(len(coverage))
+    covered_on_row = set()
+    for i in range(len(sensor_positions)):
+        sensor_x, sensor_y = sensor_positions[i]
+        radius = radii[i]
+        diff_y = abs(sensor_y - row)
+        if diff_y <= radius:
+            delta_x = abs(diff_y - radius)
+            x = sensor_x - delta_x
+            while x <= sensor_x + delta_x:
+                point = (x, row)
+                if not point in beacon_positions:
+                    covered_on_row.add(point)
+                x += 1
+
+    print(len(covered_on_row))
 
 
 if __name__ == '__main__':
