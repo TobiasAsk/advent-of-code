143e1e47f1236b3703b35c01f35d2af40562a0d1
Tobias Ask <tobias.ask.92@gmail.com>
2023-08-30 23:13:47 +0200

    Correct on example input in a few seconds

--- day_17/solution.py
+++ day_17/solution.py
@@ -31,46 +31,58 @@ ROCKS = [  # X, Y positions relative to bottom left corner. X is from left to ri
 
 CHAMBER_WIDTH = 7
 TARGET_NUM_ROCKS = 2022
-ROCK_SPAWN_DELTA = (2, 4)
+ROCK_SPAWN_DELTA_X = 2
+ROCK_SPAWN_DELTA_Y = 4
 
 
 def simulate(jet_pattern):
-    rock_idx, jet_idx, num_rocks_at_rest, tower_height = 0, 0, 0, 0
-    ground = []
-    
+    jet_idx, num_rocks_at_rest, tower_height = 0, 0, 0
+    rock_tower = set()
+
     while num_rocks_at_rest < TARGET_NUM_ROCKS:
-        spawn_delta_x, spawn_delta_y = ROCK_SPAWN_DELTA
-        rock_coordinates = [(x+spawn_delta_x, y+spawn_delta_y+tower_height)
-                            for (x, y) in ROCKS[rock_idx]]
-        rock_at_rest = False
-        while not rock_at_rest:
-            # simulate one timestep of rock movement
+        rock_coordinates = [(x+ROCK_SPAWN_DELTA_X, y+ROCK_SPAWN_DELTA_Y+tower_height)
+                            for (x, y) in ROCKS[num_rocks_at_rest % 5]]
+        falling = False
+
+        while True:
             if falling:
-                rock_coordinates = [(x, y-1)
-                                    for (x, y) in rock_coordinates]
+                next_rock_coordinates = [(x, y-1)
+                                         for (x, y) in rock_coordinates]
             else:
                 jet_push = jet_pattern[jet_idx]
-                delta_x = (1 if jet_push == '>' and all(
-                    x+1 < CHAMBER_WIDTH for (x, _) in rock_coordinates) else -1 if jet_push == '<' and all(
-                    x-1 >= 0 for (x, _) in rock_coordinates) else 0)
-
-                if delta_x != 0:
-                    rock_coordinates = [(x+delta_x, y)
-                                        for (x, y) in rock_coordinates]
-
+                delta_x = 1 if jet_push == '>' else -1
+                next_rock_coordinates = [(x+delta_x, y)
+                                         for (x, y) in rock_coordinates]
                 jet_idx = (jet_idx + 1) % len(jet_pattern)
 
-            rock_at_rest = (any(y == 0 for (_, y) in rock_coordinates) or any(y == ground_y for (
-                _, y) in rock_coordinates for (_, ground_y) in ground))
+            lowest_rock_point = min(p[1] for p in next_rock_coordinates)
+
+            if lowest_rock_point <= tower_height:
+                tower_points = set(
+                    p for p in rock_tower if p[1] >= lowest_rock_point)
+                collision_with_tower = any(
+                    point in tower_points for point in next_rock_coordinates)
+            else:
+                collision_with_tower = False
+
+            chamber_collision = any(
+                x < 0 or x >= CHAMBER_WIDTH for (x, _) in next_rock_coordinates)
+
+            collision_with_ground = lowest_rock_point == 0
+
+            if not (collision_with_tower or chamber_collision or collision_with_ground):
+                rock_coordinates = next_rock_coordinates
+            else:
+                if falling:
+                    break
+
             falling = not falling
 
-        # place rock at ground
-        ground.extend((x, y+1)
-                      for (x, y) in rock_coordinates)
-        tower_height = max(y for (_, y) in ground)
-
+        rock_tower = rock_tower | {(x, y) for (x, y) in rock_coordinates}
+        highest_rock_point = max(y for (_, y) in rock_coordinates)
+        tower_height = max(tower_height, highest_rock_point)
         num_rocks_at_rest += 1
-        rock_idx = (rock_idx + 1) % len(ROCKS)
+
     return tower_height
 
 
@@ -80,6 +92,7 @@ def main():
         jet_pattern = input.readline()
         tower_height = simulate(jet_pattern)
         print(f'Tower height is {tower_height}')
+
 
 if __name__ == '__main__':
     main()
