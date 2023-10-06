3cdcfdbd7da41c701220c5bd94355e8cd7624930
Tobias Ask <tobias.ask.92@gmail.com>
2023-10-06 18:45:53 +0200

    Save for robot and jump

--- day_19/solution.py
+++ day_19/solution.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import math
 from functools import cache
 
 BLUEPRINT_REGEX = re.compile(r'(\d+)')
@@ -13,28 +14,45 @@ def get_max_num_geodes(
         remaining_minutes: int) -> int:
 
     if remaining_minutes == 0:
-        return resources[3]
+        return 0
 
-    resources_no_build = tuple(resources[r]+robots[r]
-                               for r in range(len(robots)))
-    build_options = [(resources_no_build, robots)]
+    options = []
     for robot_type, robot_cost in enumerate(costs):
-        if all(resources[r] >= robot_cost[r]
-               for r in range(len(resources))):
-
+        # can we build this robot right now?
+        if all(resources[r] >= robot_cost[r] for r in range(len(resources))):
             resources_after_build = tuple(
                 resources[r]-robot_cost[r]+robots[r] for r in range(len(resources)))
 
             robots_after_build = tuple(
                 robots[r]+1 if r == robot_type else robots[r] for r in range(len(robots)))
 
-            build_options.append((resources_after_build, robots_after_build))
+            options.append((resources_after_build, robots_after_build, 1))
 
-    return max([get_max_num_geodes(
+        # can we save up for it?
+        elif all(robots[r] > 0 for r in range(len(resources)) if robot_cost[r] > 0):
+            required_wait_time = 0
+
+            for r in range(len(resources)):
+                wait_time = math.ceil(
+                    (robot_cost[r] - resources[r]) / robots[r]) + 1 if robots[r] > 0 else 0
+                required_wait_time = max(required_wait_time, wait_time)
+
+            if remaining_minutes - required_wait_time > 0:
+                resources_after_build = tuple(
+                    resources[r]-robot_cost[r]+robots[r]*required_wait_time for r in range(len(resources)))
+
+                robots_after_build = tuple(
+                    robots[r]+1 if r == robot_type else robots[r] for r in range(len(robots)))
+
+                options.append(
+                    (resources_after_build, robots_after_build, required_wait_time))
+
+    current = resources[3] + (robots[3]*remaining_minutes)
+    return current + max([get_max_num_geodes(
         costs=costs,
         resources=res,
         robots=rob,
-        remaining_minutes=remaining_minutes-1) for res, rob in build_options])
+        remaining_minutes=remaining_minutes-wait) for res, rob, wait in options]) if options else current
 
 
 def main():
