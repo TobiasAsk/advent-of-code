1bdbe84758f22dfdf82710fa29c59b9ca87fafab
Tobias Ask <tobias.ask.92@gmail.com>
2023-10-08 20:17:07 +0200

    Handle duplicates, but still wrong

--- day_20/solution.py
+++ day_20/solution.py
@@ -1,16 +1,29 @@
 import sys
 
 
-def move(elem_idx: int, num_moves: int, elements: list):
-    new_idx = (elem_idx + num_moves) % (len(elements)-1)
+def move(number: int, numbers: list):
+    number_indices = [i for i in range(len(numbers)) if numbers[i] == number]
+    num_steps = number
+    indices = list(reversed(number_indices)
+                   ) if num_steps > 0 else list(number_indices)
 
-    if num_moves < 0 and new_idx == 0:
-        new_idx = len(elements)
-    elif num_moves > 0 and new_idx == len(elements)-1:
-        new_idx = 0
+    for i in range(len(indices)):
+        number_idx = indices[i]
+        new_idx = (number_idx + num_steps) % (len(numbers)-1)
 
-    elem = elements.pop(elem_idx)
-    elements.insert(new_idx, elem)
+        if num_steps < 0 and new_idx == 0:
+            new_idx = len(numbers)
+        elif num_steps > 0 and new_idx == len(numbers)-1:
+            new_idx = 0
+
+        popped = numbers.pop(number_idx)
+        numbers.insert(new_idx, popped)
+
+        for j in range(i+1, len(indices)):
+            if indices[j] > new_idx and number_idx > indices[j]:
+                indices[j] += 1
+            elif indices[j] < new_idx and number_idx < indices[j]:
+                indices[j] -= 1
 
 
 def main():
@@ -20,9 +33,7 @@ def main():
 
     numbers = list(original_numbers)
     for number in original_numbers:
-        # this makes it O(n^2). Might be OK, else some index tracking is needed
-        number_idx = numbers.index(number)
-        move(elem_idx=number_idx, num_moves=number, elements=numbers)
+        move(number=number, numbers=numbers)
 
     start_idx = numbers.index(0)
     coordinate_sum = sum(numbers[(start_idx+offset) % len(numbers)]
@@ -30,9 +41,8 @@ def main():
     print(f'Coordinate sum is {coordinate_sum}')
 
 
-def test(elem: int, nums: list, expected: list):
-    elem_idx = nums.index(elem)
-    move(elem_idx, elem, nums)
+def test(number: int, nums: list, expected: list):
+    move(number, nums)
     assert nums == expected, f'Expected {expected}, got {nums}'
 
 
@@ -45,7 +55,25 @@ if __name__ == '__main__':
         (2, [1, 1, 1, 2, 1, 1, 1], [1, 1, 1, 1, 1, 2, 1]),
         (-10, [1, 1, 1, -10, 1, 1], [1, 1, 1, -10, 1, 1]),
         (10, [1, 1, 1, 10, 1, 1], [1, 1, 1, 10, 1, 1]),
+
+        # duplicates
+        (5,
+         [5, 1, 2, 5, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+         [1, 2, 3, 4, 6, 5, 7, 8, 5, 9, 10, 11, 12, 13, 14]),
+
+        (-3,
+         [1, 2, 3, 4, -3, 5, 6, -3],
+         [1, -3, 2, 3, -3, 4, 5, 6]),
+
+        (4,
+         [1, 2, 4, 3, 5, 4, 6, 7, 8],
+         [1, 4, 2, 3, 5, 6, 7, 4, 8]),
+
+        (-3,
+         [1, 1, -3, 1, 1, 1, 1, 1, -3, 1, 1, 1],
+         [1, 1, 1, 1, -3, 1, 1, 1, 1, 1, -3, 1]),
     ]
+
     for elem, nums, expected in test_cases:
         test(elem, nums, expected)
     print('Tests pass')
