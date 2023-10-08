afe28ad2e43d979c2a97c98bbe23c3fee762dc92
Tobias Ask <tobias.ask.92@gmail.com>
2023-10-08 22:17:00 +0200

    Part 1 works!

--- day_20/solution.py
+++ day_20/solution.py
@@ -19,11 +19,15 @@ def move(idx: int, numbers: list):
 def main():
     filename = sys.argv[1]
     with open(filename) as number_list:
-        original_numbers = [int(n) for n in number_list]
+        numbers = [int(n) for n in number_list]
 
-    numbers = list(original_numbers)
-    for number in original_numbers:
-        move(number=number, numbers=numbers)
+    q = collections.deque(range(len(numbers)))
+    while q:
+        idx = q.popleft()
+        new_idx = move(idx, numbers)
+        for i in range(len(q)):
+            if q[i] <= new_idx:
+                q[i] -= 1
 
     start_idx = numbers.index(0)
     coordinate_sum = sum(numbers[(start_idx+offset) % len(numbers)]
@@ -31,54 +35,5 @@ def main():
     print(f'Coordinate sum is {coordinate_sum}')
 
 
-def test(number: int, nums: list, expected: list):
-    move(number, nums)
-    assert nums == expected, f'Expected {expected}, got {nums}'
-
-
-def test2():
-    nums = [3, 5, 8, 4, 3]
-    q = collections.deque(range(len(nums)))
-    while q:
-        idx = q.popleft()
-        new_idx = move(idx, nums)
-        for i in range(len(q)):
-            if q[i] <= new_idx:
-                q[i] -= 1
-    print(nums)
-
 if __name__ == '__main__':
-    test_cases = [
-        (-2, [1, 2, -2, -3, 0, 3, 4], [1, 2, -3, 0, 3, 4, -2]),
-        (-3, [1, -3, 2, 3, -2, 0, 4], [1, 2, 3, -2, -3, 0, 4]),
-        (3, [1, -3, 2, 3, -2, 0, 4], [3, 1, -3, 2, -2, 0, 4]),
-        (2, [1, 1, 1, 2, 1, 1], [2, 1, 1, 1, 1, 1]),
-        (2, [1, 1, 1, 2, 1, 1, 1], [1, 1, 1, 1, 1, 2, 1]),
-        (-10, [1, 1, 1, -10, 1, 1], [1, 1, 1, -10, 1, 1]),
-        (10, [1, 1, 1, 10, 1, 1], [1, 1, 1, 10, 1, 1]),
-
-        # duplicates
-        (5,
-         [5, 1, 2, 5, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-         [1, 2, 3, 4, 6, 5, 7, 8, 5, 9, 10, 11, 12, 13, 14]),
-
-        (-3,
-         [1, 2, 3, 4, -3, 5, 6, -3],
-         [1, -3, 2, 3, -3, 4, 5, 6]),
-
-        (4,
-         [1, 2, 4, 3, 5, 4, 6, 7, 8],
-         [1, 4, 2, 3, 5, 6, 7, 4, 8]),
-
-        (-3,
-         [1, 1, -3, 1, 1, 1, 1, 1, -3, 1, 1, 1],
-         [1, 1, 1, 1, -3, 1, 1, 1, 1, 1, -3, 1]),
-    ]
-
-    # for elem, nums, expected in test_cases:
-    #     test(elem, nums, expected)
-
-    test2()
-    # print('Tests pass')
-
-    # main()
+    main()
