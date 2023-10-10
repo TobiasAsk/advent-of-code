81310f3e03456d48b2fbf0796b3a422e265d7541
Tobias Ask <tobiasask@microsoft.com>
2023-10-10 20:53:29 +0200

    Almost there, but fails full input

--- day_21/solution.py
+++ day_21/solution.py
@@ -57,13 +57,11 @@ def build_tree(postfix: str) -> Node:
     return stack.pop()
 
 
-def apply_inverted_operation(root: Node, operator: Node):
-    new_subtree_root: Node = (operator.left_operand if operator.left_operand.is_operator()
-                              else operator.right_operand)
+def apply_inverted_operation(root: Node, operator: Node, keep_left):
+    new_subtree_root = operator.left_operand if keep_left else operator.right_operand
     root.left_operand = new_subtree_root
     operator.value = INVERSIONS[operator.value]
-    operand = (operator.left_operand if not operator.left_operand.is_operator()
-               else operator.right_operand)
+    operand = operator.right_operand if keep_left else operator.left_operand
 
     new_subtree_rhs = root.right_operand
     root.right_operand = operator
@@ -73,35 +71,55 @@ def apply_inverted_operation(root: Node, operator: Node):
 
 def main():
     filename = sys.argv[1]
-    jobs = {}
+    jobs: dict[str, str] = {}
     with open(filename) as job_list:
         for line in job_list:
             monkey_name, job = line.split(':')
             jobs[monkey_name] = job.strip()
     jobs['humn'] = 'humn'
+    jobs['root'] = jobs['root'].replace('+', '=')
     postfix = expand_in_postfix(jobs['root'], jobs)
     tree = build_tree(postfix)
-    g = 2
+    path = search('humn', tree)
+    for operator, pick_right_child in path[1:-1]:
+        apply_inverted_operation(tree, operator, pick_right_child)
+    solution = solve(tree.right_operand)
+    print(solution)
 
+
+def solve(subtree: Node):
+    if not subtree.is_operator():
+        return int(subtree.value)
+    left_operand = solve(subtree.left_operand)
+    right_operand = solve(subtree.right_operand)
+    if subtree.value == '/':
+        return left_operand // right_operand
+    elif subtree.value == '-':
+        return left_operand - right_operand
+    elif subtree.value == '+':
+        return left_operand + right_operand
+    elif subtree.value == '*':
+        return left_operand * right_operand
+
+def search(target: str, node: Node):
+    if node.value == target:
+        return [(node, None)]
+    first_option = search(target, node.left_operand) if node.left_operand else None
+    if first_option:
+        return [(node, True)] + first_option
+    second_option = search(target, node.right_operand) if node.right_operand else None
+    if second_option: return [(node, False)] + second_option
+    return None
 
 def test():
-    jobs = {
-        'root': 'a = b',
-        'a': 'c * d',
-        'b': '3',
-        'c': '2',
-        'd': 'e + f',
-        'e': '1',
-        'f': '1'
-    }
-    postfix = expand_in_postfix(jobs['root'], jobs)
-    tree = build_tree('2 3 X + * 10 =')
-    while not tree.left_operand.value == 'X':
-        apply_inverted_operation(tree, tree.left_operand)
-    a = 2
-
+    tree = build_tree('2 1 + 3 X + * 12 =')
+    path = search('X', tree)
+    for operator, pick_right_child in path[1:-1]:
+        apply_inverted_operation(tree, operator, pick_right_child)
+    solution = solve(tree.right_operand)
+    assert solution == 1
 
 
 if __name__ == '__main__':
-    test()
+    # test()
     main()
