be76ab9bd856346ff1333a2f2f4756bc8c91d460
Tobias Ask <tobias.ask.92@gmail.com>
2023-10-23 18:58:28 +0200

    Use sets and smarter proposal uniqueness check

--- day_23/solution.py
+++ day_23/solution.py
@@ -19,7 +19,7 @@ ALL_DIRS = list(Direction)
 
 
 def get_proposals(
-        elf_positions: list[tuple[int]],
+        elf_positions: set[tuple[int]],
         directions: list[Direction]) -> dict[tuple[int], Direction]:
 
     proposals = {}
@@ -33,42 +33,20 @@ def get_proposals(
                 if all((elf_x+dx, elf_y+dy) not in elf_positions
                        for dx, dy in [d.value for d in dirs]):
 
-                    proposals[(elf_x, elf_y)] = direction
+                    dx, dy = direction.value
+                    proposals[(elf_x, elf_y)] = (elf_x+dx, elf_y+dy)
                     break
 
     return proposals
 
 
-def get_destination(position, proposals):
-    x, y = position
-    dx, dy = proposals[position].value
-    return x+dx, y+dy
-
-
 def apply_proposals(
-        proposals: dict[tuple[int], Direction],
-        elf_positions: list[tuple[int]]):
-    final_positions = list(elf_positions)
-
-    for i, position in enumerate(elf_positions):
-        if position in proposals:
-            destination = get_destination(position, proposals)
-            other_positions = elf_positions[0:i] + elf_positions[i+1:]
-            other_destinations = [get_destination(p, proposals) for p in other_positions
-                                  if p in proposals]
-
-            if destination not in other_destinations:
-                final_positions[i] = destination
-
-    return final_positions
-
-
-def print_positions(elf_positions):
-    for row in range(12):
-        for col in range(14):
-            print('#' if (col, row) in elf_positions else '.', end='')
-        print()
-    print()
+        proposals: dict[tuple[int], tuple[int]],
+        elf_positions: set[tuple[int]]):
+    proposed_destinations = list(proposals.values())
+    counts = {d: proposed_destinations.count(d) for d in proposed_destinations}
+    return {proposals[p] if p in proposals and counts[proposals[p]] == 1
+            else p for p in elf_positions}
 
 
 def main():
@@ -78,27 +56,23 @@ def main():
 
     height = len(grove_scan)
     width = len(grove_scan[0])
-    elf_positions = [(col, row) for row in range(height) for col in range(width)
-                     if grove_scan[row][col] == '#']
+    elf_positions = set((col, row) for row in range(height) for col in range(width)
+                        if grove_scan[row][col] == '#')
 
+    previous_positions = set(elf_positions)
     directions = deque([Direction.N, Direction.S, Direction.W, Direction.E])
-    for _ in range(10):
+    num_rounds = 0
+
+    while True:
         proposals = get_proposals(elf_positions, directions)
         elf_positions = apply_proposals(proposals, elf_positions)
+        num_rounds += 1
+        if previous_positions == elf_positions:
+            break
+        previous_positions = elf_positions
         directions.rotate(-1)
 
-    min_x = min(x for x, _ in elf_positions)
-    max_x = max(x for x, _ in elf_positions)
-    min_y = min(y for _, y in elf_positions)
-    max_y = max(y for _, y in elf_positions)
-    num_empty_tiles = 0
-
-    for y in range(min_y, max_y+1):
-        for x in range(min_x, max_x+1):
-            if (x, y) not in elf_positions:
-                num_empty_tiles += 1
-
-    print(num_empty_tiles)
+    print(num_rounds)
 
 
 if __name__ == '__main__':
