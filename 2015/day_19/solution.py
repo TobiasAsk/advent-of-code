66da87eb0d72af5fb118dd1bd9162ea1a207300d
Tobias Ask <tobias.ask.92@gmail.com>
2023-12-03 09:33:53 +0100

    Start part 2

--- 2015/day_19/solution.py
+++ 2015/day_19/solution.py
@@ -2,28 +2,58 @@ import sys
 from collections import defaultdict
 
 
-def main():
-    filename = sys.argv[1]
+def parse_input(input_lines):
     replacements = defaultdict(list)
 
-    with open(filename) as molecule_input_file:
-        for line in molecule_input_file:
-            if len(parts := line.split('=>')) == 2:
-                in_molecule, out_molecule = parts
-                replacements[in_molecule.strip()].append(out_molecule.strip())
-            elif line:
-                start_molecule = line
+    for line in input_lines:
+        if len(parts := line.split('=>')) == 2:
+            in_molecule, out_molecule = parts
+            replacements[in_molecule.strip()].append(out_molecule.strip())
 
-    possible_molecules = set()
+        elif line:
+            medicine_molecule = line
+
+    return replacements, medicine_molecule
+
+
+def get_min_required_steps(molecule, medicine_molecule, replacements):
+    possible_replacements = []
     for in_molecule in replacements:
         idx = 0
-        while (idx := start_molecule.find(in_molecule, idx)) > -1:
+        while (idx := molecule.find(in_molecule, idx)) > -1:
             for repl in replacements[in_molecule]:
-                new_mol = start_molecule[:idx] + repl + start_molecule[idx+len(in_molecule):]
-                possible_molecules.add(new_mol)
+                new_mol = molecule[:idx] + repl + molecule[idx+len(in_molecule):]
+
+                if new_mol == medicine_molecule:
+                    return 1
+
+                elif len(new_mol) <= len(medicine_molecule):
+                    possible_replacements.append(new_mol)
             idx += 1
 
-    print(f'Num distinct molecules is {len(possible_molecules)}')
+    if not possible_replacements:
+        return None
+
+    possibilities = [get_min_required_steps(
+        molecule=replacement,
+        medicine_molecule=medicine_molecule,
+        replacements=replacements) for replacement in possible_replacements]
+
+    return 1 + min(possibilities)
+
+
+def main():
+    filename = sys.argv[1]
+
+    with open(filename) as molecule_input_file:
+        replacements, medicine_molecule = parse_input(molecule_input_file.read().splitlines())
+
+    min_steps = get_min_required_steps(
+        molecule='e',
+        medicine_molecule='HOH',
+        replacements=replacements
+    )
+    print(min_steps)
 
 
 if __name__ == '__main__':
