5fe3339edbe9157f53b0c9c9b247596214802e6f
Tobias Ask <tobiasask@microsoft.com>
2023-12-04 10:58:37 +0100

    Part 2

--- 2023/day_04/solution.py
+++ 2023/day_04/solution.py
@@ -1,18 +1,26 @@
 import sys
 
 
+def get_num_winning_numbers(card_line):
+    parts = card_line.split('|')
+    winning_numbers = set(int(n) for n in parts[0].split(':')[1].split())
+    my_numbers = set(int(n) for n in parts[1].split())
+    return len(winning_numbers & my_numbers)
+
+
 def main():
     filename = sys.argv[1]
-    total_points = 0
+
     with open(filename) as scratchcard_file:
-        for card_line in scratchcard_file:
-            parts = card_line.split('|')
-            winning_numbers = set(int(n) for n in parts[0].split(':')[1].split())
-            my_numbers = set(int(n) for n in parts[1].split())
-            num_winning_numbers = len(winning_numbers & my_numbers)
-            if num_winning_numbers > 0:
-                total_points += 2**(num_winning_numbers-1)
-    print(total_points)
+        card_lines = scratchcard_file.readlines()
+
+    cards = [1] * len(card_lines)
+    for card_num, card_line in enumerate(card_lines):
+        num_winning_nums = get_num_winning_numbers(card_line)
+        for i in range(card_num+1, card_num+1+num_winning_nums):
+            cards[i] += cards[card_num]
+
+    print(sum(cards))
 
 
 if __name__ == '__main__':
