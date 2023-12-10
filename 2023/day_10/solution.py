a528347af87539fe3b5023f69081122f0559082d
Tobias Ask <tobias.ask.92@gmail.com>
2023-12-10 15:45:50 +0100

    Almost there

--- 2023/day_10/solution.py
+++ 2023/day_10/solution.py
@@ -73,6 +73,54 @@ def move(position: tuple[int],
     return (x, y), direction
 
 
+def rotate(direction: MovementDirection, clockwise: bool) -> MovementDirection:
+    all_dirs = list(MovementDirection)
+    dir_idx = all_dirs.index(direction)
+    offset = 1 if clockwise else -1
+    return all_dirs[(dir_idx+offset) % len(all_dirs)]
+
+
+def flood_fill(
+        position: tuple[int],
+        pipe_map: list[str],
+        loop_tiles: set[tuple[int]]) -> set[tuple[int]]:
+    q = [position]
+    tiles = set()
+    height, width = len(pipe_map), len(pipe_map[0])
+
+    while q:
+        x, y = q.pop()
+        if (x, y) not in tiles:
+            tiles.add((x, y))
+
+            for direction in list(MovementDirection):
+                dx, dy = direction.value
+                if (0 <= y+dy < height and 0 <= x+dx < width and
+                        (x+dx, y+dy) not in loop_tiles and (x+dx, y+dy) not in tiles):
+                    q.append((x+dx, y+dy))
+
+    return tiles
+
+
+def get_loop_info(start_pos, start_dir, pipe_map) -> tuple[bool, set[tuple[int]]]:
+    position = start_pos
+    direction = start_dir
+    turn_sum = 0
+    all_dirs = list(MovementDirection)
+    loop_tiles = {position}
+
+    while pipe_map[position[1]][position[0]] != 'S':
+        previous_direction = direction
+        position, direction = move(position, direction, pipe_map)
+        if direction != previous_direction:
+            prev_dir_idx = all_dirs.index(previous_direction)
+            turn_diff = 1 if all_dirs[(prev_dir_idx+1) % 4] == direction else -1
+            turn_sum += turn_diff
+        loop_tiles.add(position)
+
+    return turn_sum > 0, loop_tiles
+
+
 def main():
     filename = sys.argv[1]
     with open(filename) as pipe_map_file:
@@ -81,13 +129,25 @@ def main():
     start_pos = get_starting_position(pipe_map)
     start_direction = get_starting_direction(start_pos, pipe_map)
     position, direction = move(start_pos, start_direction, pipe_map)
-    loop_length = 1
+    height, width = len(pipe_map), len(pipe_map[0])
+
+    is_clockwise, loop_tiles = get_loop_info(position, direction, pipe_map)
+    enclosed_tiles = set()
 
     while pipe_map[position[1]][position[0]] != 'S':
-        position, direction = move(position, direction, pipe_map)
-        loop_length += 1
+        inside_dir = rotate(direction, clockwise=is_clockwise)
+        x, y = position
+        rhs_dx, rhs_dy = inside_dir.value
+        within_bounds = 0 <= y+rhs_dy < height and 0 <= x+rhs_dx < width
 
-    print(loop_length // 2)
+        if (within_bounds and (x+rhs_dx, y+rhs_dy) not in loop_tiles
+                and (x+rhs_dx, y+rhs_dy) not in enclosed_tiles):
+            tiles_in_segment = flood_fill((x+rhs_dx, y+rhs_dy), pipe_map, loop_tiles)
+            enclosed_tiles |= tiles_in_segment
+
+        position, direction = move(position, direction, pipe_map)
+
+    print(len(enclosed_tiles))
 
 
 if __name__ == '__main__':
