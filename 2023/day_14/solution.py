b6616ca3fd507150b1b8629b8e8d7a3634f961bd
Tobias Ask <tobias.ask.92@gmail.com>
2024-01-04 22:28:05 +0100

    Sensible tilt, use 2d bool array

--- 2023/day_14/solution.py
+++ 2023/day_14/solution.py
@@ -8,18 +8,19 @@ TILT_DIRECTIONS = [
 ]
 
 
-def parse_rock_map(map_lines: list[str]) -> tuple[set[tuple[int]]]:
+def parse_rock_map(map_lines: list[str]) -> list[tuple[int]]:
     height = len(map_lines)
     width = len(map_lines[0])
     rolling_rocks = [(x, y) for y in range(height) for x in range(width) if
                      map_lines[y][x] == 'O']
     static_rocks = [(x, y) for y in range(height) for x in range(width) if
                     map_lines[y][x] == '#']
-    return rolling_rocks, static_rocks, (height, width)
+    free = [[True if map_lines[y][x] == '.' else False for x in range(width)] for y in range(height)]
+    return rolling_rocks, static_rocks, free
 
 
-def print_rocks(rolling_rocks, static_rocks, dimensions):
-    height, width = dimensions
+def print_rocks(rolling_rocks, static_rocks, map_rows):
+    height, width = len(map_rows), len(map_rows[0])
     for y in range(height):
         print(''.join('O' if (x, y) in rolling_rocks else
                       '#' if (x, y) in static_rocks else
@@ -31,27 +32,40 @@ def calculate_load(rolling_rocks, platform_height):
     return sum(platform_height - y for _, y in rolling_rocks)
 
 
-def tilt(rolling_rocks, static_rocks, direction, platform_dimensions):
-    height, width = platform_dimensions
+def tilt(rolling_rocks, direction, free: list[list[bool]]):
+    height, width = len(free), len(free[0])
     dx, dy = direction
-    moving = True
+    dir_idx = TILT_DIRECTIONS.index(direction)
 
-    while moving:
-        moving = False
-        for i in range(len(rolling_rocks)):
-            rock_x, rock_y = rolling_rocks[i]
-            new_rock_x, new_rock_y = rock_x + dx, rock_y + dy
-            if (0 <= new_rock_x < width and 0 <= new_rock_y < height and
-                    (new_rock_x, new_rock_y) not in rolling_rocks + static_rocks):
-                rolling_rocks[i] = (new_rock_x, new_rock_y)
-                moving = True
+    if dir_idx == 0:
+        rolling_rocks = sorted(rolling_rocks, key=lambda pos: pos[1])
+    elif dir_idx == 1:
+        rolling_rocks = sorted(rolling_rocks, key=lambda pos: pos[0])
+    elif dir_idx == 2:
+        rolling_rocks = sorted(rolling_rocks, key=lambda pos: pos[1], reverse=True)
+    elif dir_idx == 3:
+        rolling_rocks = sorted(rolling_rocks, key=lambda pos: pos[0], reverse=True)
+
+    for i in range(len(rolling_rocks)):
+        rock_x, rock_y = rolling_rocks[i]
+        new_rock_x, new_rock_y = rock_x, rock_y
+        while (0 <= new_rock_x + dx < width and 0 <= new_rock_y + dy < height and
+                free[new_rock_y+dy][new_rock_x+dx]):
+            new_rock_x += dx
+            new_rock_y += dy
+
+        rolling_rocks[i] = (new_rock_x, new_rock_y)
+        free[rock_y][rock_x] = True
+        free[new_rock_y][new_rock_x] = False
+    return rolling_rocks
 
 
 def main():
     filename = sys.argv[1]
     with open(filename) as map_file:
-        rolling_rocks, static_rocks, dimensions = parse_rock_map(map_file.read().splitlines())
+        map_rows = map_file.read().splitlines()
 
+    rolling_rocks, static_rocks, free = parse_rock_map(map_rows)
     target_num_cycles = 1000000000
     rock_hashes = []
     cycles = 0
@@ -59,7 +73,7 @@ def main():
 
     while cycles < target_num_cycles:
         for direction in TILT_DIRECTIONS:
-            tilt(rolling_rocks, static_rocks, direction, dimensions)
+            rolling_rocks = tilt(rolling_rocks, direction, free)
 
         cycles += 1
         rock_hash = hash(tuple(sorted(rolling_rocks)))
@@ -72,7 +86,7 @@ def main():
 
         rock_hashes.append(rock_hash)
 
-    load = calculate_load(rolling_rocks, dimensions[0])
+    load = calculate_load(rolling_rocks, len(map_rows))
     print(load)
 
 
