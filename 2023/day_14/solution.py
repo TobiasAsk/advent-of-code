d4b7295d612f0a1f08b10d9fe1a3b2b8ded20b11
Tobias Ask <tobias.ask.92@gmail.com>
2024-01-04 14:47:22 +0100

    Off by one. Also terribly inefficient

--- 2023/day_14/solution.py
+++ 2023/day_14/solution.py
@@ -1,5 +1,12 @@
 import sys
 
+TILT_DIRECTIONS = [
+    (0, -1),
+    (-1, 0),
+    (0, 1),
+    (1, 0)
+]
+
 
 def parse_rock_map(map_lines: list[str]) -> tuple[set[tuple[int]]]:
     height = len(map_lines)
@@ -14,28 +21,59 @@ def parse_rock_map(map_lines: list[str]) -> tuple[set[tuple[int]]]:
 def print_rocks(rolling_rocks, static_rocks, dimensions):
     height, width = dimensions
     for y in range(height):
-        print(''.join('O' if (x, y) in rolling_rocks else '.' for x in range(width)))
+        print(''.join('O' if (x, y) in rolling_rocks else
+                      '#' if (x, y) in static_rocks else
+                      '.' for x in range(width)))
+    print()
 
 
 def calculate_load(rolling_rocks, platform_height):
-    def load(rock_y): return platform_height - rock_y
-    return sum(load(y) for _, y in rolling_rocks)
+    return sum(platform_height - y for _, y in rolling_rocks)
+
+
+def tilt(rolling_rocks, static_rocks, direction, platform_dimensions):
+    height, width = platform_dimensions
+    dx, dy = direction
+    moving = True
+
+    while moving:
+        moving = False
+        for i in range(len(rolling_rocks)):
+            rock_x, rock_y = rolling_rocks[i]
+            new_rock_x, new_rock_y = rock_x + dx, rock_y + dy
+            if (0 <= new_rock_x < width and 0 <= new_rock_y < height and
+                    (new_rock_x, new_rock_y) not in rolling_rocks + static_rocks):
+                rolling_rocks[i] = (new_rock_x, new_rock_y)
+                moving = True
 
 
 def main():
     filename = sys.argv[1]
     with open(filename) as map_file:
-        rolling_rocks, static_rocks, dimensions = parse_rock_map(map_file.readlines())
+        rolling_rocks, static_rocks, dimensions = parse_rock_map(map_file.read().splitlines())
 
-    moving = True
-    while moving:
-        moving = False
-        for i in range(len(rolling_rocks)):
-            rock_x, rock_y = rolling_rocks[i]
-            if rock_y - 1 >= 0 and (rock_x, rock_y-1) not in rolling_rocks + static_rocks:
-                rolling_rocks[i] = (rock_x, rock_y-1)
-                moving = True
+    target_num_cycles = 1000000000
+    rock_hashes = set()
+    cycles = 0
+    skipped = False
+    print_rocks(rolling_rocks, static_rocks, dimensions)
 
+    while cycles < target_num_cycles:
+        for direction in TILT_DIRECTIONS:
+            tilt(rolling_rocks, static_rocks, direction, dimensions)
+            print_rocks(rolling_rocks, static_rocks, dimensions)
+
+        cycles += 1
+
+        rock_hash = hash(tuple(rolling_rocks))
+        if rock_hash in rock_hashes and not skipped:
+            num_skipped_cycles = (target_num_cycles - cycles) // cycles
+            cycles += num_skipped_cycles * cycles
+            skipped = True
+
+        rock_hashes.add(rock_hash)
+
+    print_rocks(rolling_rocks, static_rocks, dimensions)
     load = calculate_load(rolling_rocks, dimensions[0])
     print(load)
 
