cdca582d97a370db4c14bba44cdff6b1abbd445f
Tobias Ask <tobiasask@microsoft.com>
2024-12-02 15:18:44 +0100

    2024 d2 p2 - naive but ok

--- 2024/day_02/solution.py
+++ 2024/day_02/solution.py
@@ -1,6 +1,14 @@
 import sys
 
 
+def is_safe_remove_one(levels: list[int]) -> bool:
+    for i in range(len(levels)):
+        new_levels = levels[:i] + levels[i+1:]
+        if is_safe(new_levels):
+            return True
+    return False
+
+
 def is_safe(levels: list[int]) -> bool:
     increasing = None
     for i in range(1, len(levels)):
@@ -8,12 +16,12 @@ def is_safe(levels: list[int]) -> bool:
         diff = previous_level - level
         if increasing is None:
             increasing = diff > 0
-        
+
         valid_diff = (increasing and diff > 0) or (not increasing and diff < 0)
 
         if abs(diff) > 3 or not valid_diff:
             return False
-        
+
     return True
 
 
@@ -23,9 +31,13 @@ def main():
     with open(filename) as reports:
         for report in reports:
             levels = list(map(int, report.split()))
-            num_safe += is_safe(levels)
+            if is_safe(levels):
+                num_safe += 1
+            else:
+                num_safe += is_safe_remove_one(levels)
 
     print(num_safe)
+
 
 if __name__ == '__main__':
     main()
