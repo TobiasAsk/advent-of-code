51076e851f07d4eba6b9552d04726a5d81277431
Tobias Ask <tobiasask@microsoft.com>
2024-12-03 12:08:26 +0100

    2024 d3 p2

--- 2024/day_03/solution.py
+++ 2024/day_03/solution.py
@@ -1,18 +1,39 @@
 import sys
 import re
+from enum import StrEnum
 
 
-MUL_PATTERN = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
+class Token(StrEnum):
+    MUL = 'MUL'
+    DO = 'DO'
+    DONT = "DONT"
+
+
+TOKENS = [
+    (Token.MUL, r'mul\((\d{1,3}),(\d{1,3})\)'),
+    (Token.DO, r'do\(\)'),
+    (Token.DONT, r"don't\(\)")
+]
 
 
 def main():
     filename = sys.argv[1]
     with open(filename) as instructions_file:
         instructions = instructions_file.read()
-    matches = MUL_PATTERN.findall(instructions)
+
+    all_tokens = '|'.join(f'(?P<{t}>{p})' for t, p in TOKENS)
+    enabled = True
     mul_sum = 0
-    for left_op, right_op in [tuple(map(int, m)) for m in matches]:
-        mul_sum += left_op * right_op
+    for match in re.finditer(all_tokens, instructions):
+        type = match.lastgroup
+        if type == Token.MUL and enabled:
+            left_op, right_op = int(match.group(2)), int(match.group(3))
+            mul_sum += left_op * right_op
+        elif type == Token.DO:
+            enabled = True
+        elif type == Token.DONT:
+            enabled = False
+
     print(mul_sum)
 
 
