394f49318573df2d9eac2d1f3d0abd407202fc1f
Tobias Ask <tobias.ask.92@gmail.com>
2024-12-06 12:16:11 +0100

    2024 d4 p2

--- 2024/day_04/solution.py
+++ 2024/day_04/solution.py
@@ -1,16 +1,14 @@
 import sys
 
 
-DIRECTIONS = [
-    (1, 0),
-    (0, 1),
-    (1, 1),
-    (-1, 1)
-]
+def is_mas_either_direction(letters: str) -> bool:
+    return letters == 'MAS' or letters == 'SAM'
 
 
-def is_xmas_either_direction(letters: str) -> bool:
-    return letters == 'XMAS' or letters == 'SAMX'
+def has_cross_mas(box: list[str]) -> bool:
+    diag_1 = ''.join(box[i][i] for i in range(3))
+    diag_2 = ''.join(box[2-i][i] for i in range(3))
+    return is_mas_either_direction(diag_1) and is_mas_either_direction(diag_2)
 
 
 def main():
@@ -22,12 +20,10 @@ def main():
     width = len(word_search[0])
     xmas_count = 0
 
-    for y in range(height):
-        for x in range(width):
-            for dx, dy in DIRECTIONS:
-                letters = ''.join(word_search[y+dy*i][x+dx*i] for i in range(4)
-                                  if 0 <= x+dx*i < width and 0 <= y+dy*i < height)
-                xmas_count += is_xmas_either_direction(letters)
+    for y in range(1, height-1):
+        for x in range(1, width-1):
+            box = [word_search[y+i][x-1:x+2] for i in range(-1, 2)]
+            xmas_count += has_cross_mas(box)
 
     print(xmas_count)
 
