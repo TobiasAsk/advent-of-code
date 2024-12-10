54e85e6b01480d7ec7ecb7c727571990d839bb9e
Tobias Ask <tobias.ask.92@gmail.com>
2024-12-10 22:09:04 +0100

    2024 d6 p2

--- 2024/day_06/solution.py
+++ 2024/day_06/solution.py
@@ -15,12 +15,7 @@ def find_guard(lab_map: list[str]) -> tuple[int]:
             lab_map[y][x] == '^'][0]
 
 
-def main():
-    filename = sys.argv[1]
-    with open(filename) as map_file:
-        lab_map = map_file.read().splitlines()
-
-    guard_start_pos = find_guard(lab_map)
+def get_positions(lab_map: list[str], guard_start_pos: tuple[int]) -> set[tuple[int]]:
     heading = 0
     height = len(lab_map)
     width = len(lab_map[0])
@@ -44,7 +39,67 @@ def main():
 
         heading = (heading+1) % 4
 
-    print(len(positions))
+    return positions
+
+
+def is_loop_walk(guard_start_pos, lab_map) -> bool:
+    heading = 0
+    height = len(lab_map)
+    width = len(lab_map[0])
+    guard_x, guard_y = guard_start_pos
+    corners = set()
+
+    while True:
+        dx, dy = DIRECTIONS[heading]
+
+        while (0 <= guard_x+dx < width and 0 <= guard_y+dy < height
+               and lab_map[guard_y+dy][guard_x+dx] in '.^'):
+            guard_x += dx
+            guard_y += dy
+
+        if ((guard_x == 0 and heading == 3)
+            or (guard_x == width-1 and heading == 1)
+            or (guard_y == 0 and heading == 0)
+                or (guard_y == height-1 and heading == 2)):
+            return False
+
+        if (guard_x, guard_y, heading) in corners:
+            return True
+        corners.add((guard_x, guard_y, heading))
+
+        heading = (heading+1) % 4
+
+
+def print_map(lab_map):
+    for row in lab_map:
+        print(''.join(t for t in row))
+
+
+def main():
+    filename = sys.argv[1]
+    with open(filename) as map_file:
+        lab_map = map_file.read().splitlines()
+
+    guard_start_pos = find_guard(lab_map)
+    positions = get_positions(lab_map, guard_start_pos)
+
+    height = len(lab_map)
+    width = len(lab_map[0])
+
+    obstacle_coords = [(x, y) for x in range(width) for y in range(height) if
+                       lab_map[y][x] == '#']
+    obstacle_x_coords = set(x for x, _ in obstacle_coords)
+    obstacle_y_coords = set(y for _, y in obstacle_coords)
+    num_options = 0
+    for x, y in positions - {guard_start_pos}:
+        if x in obstacle_x_coords or y in obstacle_y_coords:
+            old_row = lab_map[y]
+            lab_map[y] = old_row[:x] + '#' + old_row[x+1:]
+            if is_loop_walk(guard_start_pos, lab_map):
+                num_options += 1
+            lab_map[y] = old_row
+
+    print(num_options)
 
 
 if __name__ == '__main__':
