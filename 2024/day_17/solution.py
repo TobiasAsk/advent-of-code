dc6c73893002f2e09192d9f3d41977cca7aaa733
Tobias Ask <tobias.ask.92@gmail.com>
2025-07-04 23:17:08 +0200

    Almost, but highest

--- 2024/day_17/solution.py
+++ 2024/day_17/solution.py
@@ -23,7 +23,10 @@ class Computer:
         if operand == 6:
             return self.register_values['C']
 
-    def run_program(self):
+    def run_program(self, print_output=True):
+        self.instruction_pointer = 0
+        self.output = []
+
         while self.instruction_pointer < len(self.program):
             opcode = self.program[self.instruction_pointer]
             should_increment = True
@@ -56,7 +59,49 @@ class Computer:
             if should_increment:
                 self.instruction_pointer += 2
 
-        print(','.join(str(o) for o in self.output))
+        if print_output:
+            print(','.join(str(o) for o in self.output))
+
+    def search(self, register_value: str, idx: int = 0):
+        # print(f'Trying {register_value}')
+        base_ten_value = int(register_value, 8)
+        self.register_values = {
+            'A': base_ten_value,
+            'B': 0,
+            'C': 0
+        }
+        self.run_program(print_output=False)
+        if self.output == self.program:
+            return base_ten_value
+
+        comp_idx = 15 - idx
+        if self.output[comp_idx:] != self.program[comp_idx:]:
+            return
+
+        extension = first_extension(register_value, idx)
+        while extension != None:
+            val = self.search(*extension)
+            if val:
+                return val
+            extension = next_extension(*extension)
+
+
+def next_extension(register_value: str, idx: int) -> tuple[str, int]:
+    digit = int(register_value[idx])
+    threshold = 1 if idx == 0 else 0
+
+    if digit > threshold:
+        return (register_value[:idx] + str(digit-1) + register_value[idx+1:], idx)
+
+    if idx == 15:
+        return None
+
+    next_digit = int(register_value[idx+1])
+    return (register_value[:idx+1] + str(next_digit-1) + register_value[idx+2:], idx+1)
+
+
+def first_extension(register_value: str, idx: int) -> tuple[str, int]:
+    return (register_value, idx+1) if idx < 15 else None
 
 
 def parse_register(register_info_line: str) -> tuple[str, int]:
@@ -69,11 +114,17 @@ def main():
     with open(filename) as program_info_file:
         register_info, program_instructions = program_info_file.read().split('\n\n')
 
-    register_values = dict(parse_register(r) for r in register_info.splitlines())
     program_instructions = [int(n) for n in DIGIT_PATTERN.findall(program_instructions)]
+
+    register_values = {'B': 0, 'C': 0}
+    # value = int(sys.argv[2], 8)
+    # register_values['A'] = value
     computer = Computer(register_values, program_instructions)
-    computer.run_program()
-    f = 2
+    # computer.run_program(True)
+    # print(f'Output length: {len(computer.output)}')
+
+    val = computer.search('7777777777777777')
+    print(val)
 
 
 if __name__ == '__main__':
