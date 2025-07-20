f2b2df511e4d54e624e5dc9b8d7d1696cb23e9c3
Tobias Ask <tobias.ask.92@gmail.com>
2025-07-20 20:51:25 +0200

    d20 p2

--- 2024/day_20/solution.py
+++ 2024/day_20/solution.py
@@ -35,6 +35,16 @@ def get_distances_from_end(start_pos: tuple[int], end_pos: tuple[int], racetrack
     return {p: length-i for i, p in enumerate(path)}
 
 
+def manhattan_distance(point: tuple[int], other_point: tuple[int]) -> int:
+    return abs(point[0]-other_point[0]) + abs(point[1]-other_point[1])
+
+
+def get_all_reachable(pos: tuple[int], distance: int) -> set[tuple[int]]:
+    x, y = pos
+    return {(x+dx, y+dy) for dx in range(-distance, distance+1) for dy in range(-distance, distance+1)
+            if manhattan_distance(pos, (x+dx, y+dy)) <= distance}
+
+
 def main():
     filename = sys.argv[1]
     with open(filename) as racetrack_file:
@@ -44,20 +54,25 @@ def main():
     end_pos = get_object_position(racetrack_map, 'E')
     distances_from_end = get_distances_from_end(start_pos, end_pos, racetrack_map)
 
-    width, height = len(racetrack_map[0]), len(racetrack_map)
     num_good_cheats = 0
-    for x, y in distances_from_end.keys():
-        for dx, dy in DIRECTIONS:
-            if 0 <= x+2*dx < width and 0 <= y+2*dy < height:
-                crosses_wall = (racetrack_map[y+dy][x+dx] == '#' and
-                                racetrack_map[y+2*dy][x+2*dx] in '.E')
-                if crosses_wall:
-                    diff = distances_from_end[(x, y)] - distances_from_end[(x+2*dx, y+2*dy)] - 2
-                    if diff >= 100:
-                        num_good_cheats += 1
+    path = distances_from_end.keys()
+    for pos in path:
+        twenty_dist = get_all_reachable(pos, 20)
+        tiles_within_reach = twenty_dist & path
+        for other_pos in tiles_within_reach:
+            dist_diff = distances_from_end[pos] - distances_from_end[other_pos]
+            save = dist_diff - manhattan_distance(pos, other_pos)
+            if save >= 100:
+                num_good_cheats += 1
 
     print(num_good_cheats)
 
 
+def test():
+    reachable = get_all_reachable((0, 0), 3)
+    assert len(reachable) == 25
+
+
 if __name__ == '__main__':
+    test()
     main()
