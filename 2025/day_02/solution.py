565620f0e1c2963aaf1c4b4661ba0fb5bf272627
Tobias Ask <tobias.ask.92@gmail.com>
2025-12-07 23:45:25 +0100

    d2 p2

--- 2025/day_02/solution.py
+++ 2025/day_02/solution.py
@@ -7,14 +7,16 @@ def main():
         id_ranges_raw = id_ranges_file.readline().split(',')
 
     id_ranges = [tuple(int(x) for x in r.split('-')) for r in id_ranges_raw]
-    invalid_id_sum = 0
+    invalid_ids = set()
     for start, stop in id_ranges:
         for num in range(start, stop+1):
             str_num = str(num)
             mid = len(str_num) // 2
-            if str_num[:mid] == str_num[mid:]:
-                invalid_id_sum += num
-    print(invalid_id_sum)
+            for chunk_size in range(1, mid+1):
+                chunks = [str_num[i:i+chunk_size] for i in range(0, len(str_num), chunk_size)]
+                if all(chunks[i] == chunks[i+1] for i in range(len(chunks)-1)):
+                    invalid_ids.add(num)
+    print(sum(invalid_ids))
 
 
 if __name__ == '__main__':
