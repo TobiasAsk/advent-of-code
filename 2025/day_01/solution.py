6bcfee89b412b2a12b2f84c620df539cb3a64396
Tobias Ask <tobias.ask.92@gmail.com>
2025-12-07 22:45:33 +0100

    d1 p2

--- 2025/day_01/solution.py
+++ 2025/day_01/solution.py
@@ -1,3 +1,5 @@
+'''Modulo refresher.'''
+
 import sys
 import re
 
@@ -8,16 +10,20 @@ def main():
     filename = sys.argv[1]
     with open(filename) as rotations_file:
         rotations = rotations_file.read().splitlines()
-    
+
     current_num = 50
     zero_count = 0
     for rotation in rotations:
         match = ROTATION_PATTERN.search(rotation)
         direction, num_steps = match.group(1), int(match.group(2))
-        current_num = (current_num + num_steps) % 100 if direction == 'R' else (current_num - num_steps) % 100
-        if current_num == 0:
-            zero_count += 1
+        sign = 1 if direction == 'R' else -1
+        new_num_mod_only = current_num + sign*(num_steps % 100)
+        zero_count += num_steps // 100
+        zero_count += current_num > 0 and (new_num_mod_only < 0 or new_num_mod_only > 100)
+        current_num = (current_num + sign*num_steps) % 100
+        zero_count += current_num == 0
     print(zero_count)
+
 
 if __name__ == '__main__':
     main()
