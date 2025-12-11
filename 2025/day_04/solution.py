6c05699f3b3974efa61e14e399ef8395141b7974
Tobias Ask <tobiasask@microsoft.com>
2025-12-11 14:22:11 +0100

    d4 p2

--- 2025/day_04/solution.py
+++ 2025/day_04/solution.py
@@ -18,14 +18,26 @@ def main():
 
     roll_diagram = add_padding(roll_diagram)
     height, width = len(roll_diagram), len(roll_diagram[0])
-    num_accessible = 0
-    for y in range(1, height):
-        for x in range(1, width):
-            if roll_diagram[y][x] == '@':
-                neighbors = [roll_diagram[y+dy][x+dx] for dy in range(-1, 2) for dx in range(-1, 2)
-                             if (dx, dy) != (0, 0) and roll_diagram[y+dy][x+dx] == '@']
-                num_accessible += len(neighbors) < 4
-    print(num_accessible)
+    removing = True
+    total_removed = 0
+    
+    while removing:
+        removing = False
+        new_diagram = [r for r in roll_diagram]
+        for y in range(1, height):
+            for x in range(1, width):
+                if roll_diagram[y][x] == '@':
+                    neighbors = [roll_diagram[y+dy][x+dx] for dy in range(-1, 2) for dx in range(-1, 2)
+                                 if (dx, dy) != (0, 0) and roll_diagram[y+dy][x+dx] == '@']
+
+                    if len(neighbors) < 4:
+                        new_diagram[y] = new_diagram[y][:x] + 'x' + new_diagram[y][x+1:]
+                        removing = True
+                        total_removed += 1
+
+        roll_diagram = [r for r in new_diagram]
+
+    print(total_removed)
 
 
 if __name__ == '__main__':
