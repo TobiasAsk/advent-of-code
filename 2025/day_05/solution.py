275ef653b66457ee783a2ce30196ed75482e01d6
Tobias Ask <tobiasask@microsoft.com>
2025-12-12 09:25:50 +0100

    d5 p2

--- 2025/day_05/solution.py
+++ 2025/day_05/solution.py
@@ -8,16 +8,22 @@ def is_in_range(range: tuple[int, ...], num: int) -> bool:
 def main():
     filename = sys.argv[1]
     with open(filename) as ingredient_inventory_file:
-        ingredient_ranges_raw, available_ingredients = [s.splitlines()
-                                                        for s in ingredient_inventory_file.read().split('\n\n')]
+        ingredient_ranges_raw, _ = [s.splitlines()
+                                    for s in ingredient_inventory_file.read().split('\n\n')]
         ingredient_ranges = [tuple(int(d) for d in r.split('-')) for r in ingredient_ranges_raw]
-        available_ingredients = [int(i) for i in available_ingredients]
 
-    num_fresh = 0
-    for ingredient in available_ingredients:
-        if any(is_in_range(r, ingredient) for r in ingredient_ranges):
-            num_fresh += 1
-    print(num_fresh)
+    ingredient_ranges = sorted(ingredient_ranges)
+    num_fresh_ids = 0
+    max_num = 0
+    for start, end in ingredient_ranges:
+        if end > max_num:
+            if start > max_num:
+                num_fresh_ids += end-start+1
+            else:
+                num_fresh_ids += end-max_num
+        max_num = max(max_num, end)
+
+    print(num_fresh_ids)
 
 
 if __name__ == '__main__':
